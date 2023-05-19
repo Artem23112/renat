680048b6d164f78504cc5fe774696acb3f5a7c26
Artem23112 <93328974+Artem23112@users.noreply.github.com>
2023-05-19 13:48:59 +0300

    ку

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.header-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: false,
+        nextArrow: false
+    });
+});
